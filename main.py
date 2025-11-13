ef9b9e7350ab4d2aedd7f308fc8415014880617b
allan-haciresitoglu <allan.haciresitoglu@edu.esiee.fr>
2025-11-13 09:49:41 +0000

    exo07-asciiart

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 #### Imports et définition des variables globales
+"""
+Module principal pour l'exercice d'encodage ASCII art.
 
+Ce fichier contient les implémentations itérative (artcode_i) et
+récursive (artcode_r) de la fonction d'encodage de chaîne,
+ainsi qu'une fonction main() pour les tester.
+"""
 # Mandatory for the recursive solution to work on large inputs
 import sys
 sys.setrecursionlimit(2000)
@@ -9,7 +15,8 @@ sys.setrecursionlimit(2000)
 
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples encodant une chaîne de caractères passée en 
+       argument selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -17,14 +24,23 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
 
-    return [ ]
+    # votre code ici
+    lettres = [s[0]]
+    occurence = [1]
+    for i in range(1,len(s)):
+        if s[i] == s[i-1] :
+            occurence[-1] +=1
+        else:
+            lettres.append(s[i])
+            occurence.append(1)
+    combine = zip(lettres,occurence)
+    return list(combine)
 
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """retourne la liste de tuples encodant une chaîne de caractères passée en argument 
+       selon un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -32,20 +48,34 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
+
     # votre code ici
+    if len(s) == 0 :
+        return []
+    premier_caractere = s[0]
+    compteur = 0
+    i = 0
+    while i < len(s) and s[i] == premier_caractere:
+        compteur += 1
+        i += 1
+    chaine_restante = s[i:]
+    return [(premier_caractere, compteur)] + artcode_r(chaine_restante)
 
-    # cas de base
-    # recherche nombre de caractères identiques au premier
-    # appel récursif
-
-    return []
-    
 
 #### Fonction principale
 
 
 def main():
+    """
+    Exécute des appels de test aux fonctions 'artcode_i' et 'artcode_r' 
+    pour vérifier et afficher leur sortie sur une chaîne d'exemple.
+
+    Args:
+        Aucun.
+
+    Returns:
+        Rien.
+    """
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
